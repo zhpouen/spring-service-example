f07f52f2e989d7c114ece715d129cd14ca066c40
zhoupeng <zhpouen@qq.com>
2023-06-12 11:34:30 +0800

    网关限流

--- hello-gateway-boot/src/main/java/priv/stone/demo/springservices/hellogateway/ClusterFlowRuleConfiguration.java
+++ hello-gateway-boot/src/main/java/priv/stone/demo/springservices/hellogateway/ClusterFlowRuleConfiguration.java
@@ -24,26 +24,27 @@ import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayFlowRule;
 import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayRuleManager;
 import com.alibaba.csp.sentinel.adapter.gateway.sc.SentinelGatewayFilter;
 import com.alibaba.csp.sentinel.adapter.gateway.sc.exception.SentinelGatewayBlockExceptionHandler;
+import com.alibaba.csp.sentinel.cluster.ClusterStateManager;
 import com.alibaba.csp.sentinel.cluster.client.config.ClusterClientAssignConfig;
 import com.alibaba.csp.sentinel.cluster.client.config.ClusterClientConfig;
 import com.alibaba.csp.sentinel.cluster.client.config.ClusterClientConfigManager;
 import com.alibaba.csp.sentinel.datasource.ReadableDataSource;
 import com.alibaba.csp.sentinel.datasource.nacos.NacosDataSource;
+import com.alibaba.csp.sentinel.transport.config.TransportConfig;
+import com.alibaba.csp.sentinel.util.HostNameUtil;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.TypeReference;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.codec.ServerCodecConfigurer;
 import org.springframework.web.reactive.result.view.ViewResolver;
 
 import javax.annotation.PostConstruct;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 /**
  * @author Eric Zhao
@@ -92,6 +93,7 @@ public class ClusterFlowRuleConfiguration {
         initClientConfigProperty();
         // Token client assign config (e.g. target token server) retrieved from assign map:
         initClientServerAssignProperty();
+        initStateProperty();
     }
 
     private void initCustomizedApis() {
@@ -117,17 +119,32 @@ public class ClusterFlowRuleConfiguration {
         Set<GatewayFlowRule> rules = new HashSet<>();
         rules.add(new GatewayFlowRule("echo_api")
                 .setResourceMode(SentinelGatewayConstants.RESOURCE_MODE_CUSTOM_API_NAME)
-                .setCount(5)
+                .setCount(50)
                 .setIntervalSec(1)
         );
 
         rules.add(new GatewayFlowRule("divide_api")
                 .setResourceMode(SentinelGatewayConstants.RESOURCE_MODE_CUSTOM_API_NAME)
-                .setCount(2)
+                .setCount(20)
                 .setIntervalSec(1)
         );
         GatewayRuleManager.loadRules(rules);
     }
+
+//    private void initClientConfigProperty() {
+//        ReadableDataSource<String, ClusterClientConfig> clientConfigDs = new NacosDataSource<>(remoteAddress, groupId,
+//                configDataId, source -> JSON.parseObject(source, new TypeReference<>() {}));
+//        ClusterClientConfigManager.registerClientConfigProperty(clientConfigDs.getProperty());
+//    }
+//
+//    private void initClientServerAssignProperty() {
+//        ReadableDataSource<String, ClusterClientAssignConfig> clientAssignDs = new NacosDataSource<>(remoteAddress, groupId,
+//                clusterMapDataId, source -> {
+//            List<ClusterClientAssignConfig> assignConfigs = JSON.parseObject(source, new TypeReference<>() {});
+//            return (assignConfigs == null || assignConfigs.size() == 0) ? null : assignConfigs.get(0);
+//        });
+//        ClusterClientConfigManager.registerServerAssignProperty(clientAssignDs.getProperty());
+//    }
 
     private void initClientConfigProperty() {
         ReadableDataSource<String, ClusterClientConfig> clientConfigDs = new NacosDataSource<>(remoteAddress, groupId,
@@ -138,11 +155,55 @@ public class ClusterFlowRuleConfiguration {
     private void initClientServerAssignProperty() {
         ReadableDataSource<String, ClusterClientAssignConfig> clientAssignDs = new NacosDataSource<>(remoteAddress, groupId,
                 clusterMapDataId, source -> {
-            List<ClusterClientAssignConfig> assignConfigs = JSON.parseObject(source, new TypeReference<>() {});
-            return (assignConfigs == null || assignConfigs.size() == 0) ? null : assignConfigs.get(0);
+            List<ClusterGroupEntity> groupList = JSON.parseObject(source, new TypeReference<>() {});
+            return Optional.ofNullable(groupList)
+                    .flatMap(this::extractClientAssignment)
+                    .orElse(null);
         });
         ClusterClientConfigManager.registerServerAssignProperty(clientAssignDs.getProperty());
     }
 
+    private void initStateProperty() {
+        // Cluster map format:
+        // [{"clientSet":["112.12.88.66@8729","112.12.88.67@8727"],"ip":"112.12.88.68","machineId":"112.12.88.68@8728","port":11111}]
+        // machineId: <ip@commandPort>, commandPort for port exposed to Sentinel dashboard (transport module)
+        ReadableDataSource<String, Integer> clusterModeDs = new NacosDataSource<>(remoteAddress, groupId,
+                clusterMapDataId, source -> {
+            List<ClusterGroupEntity> groupList = JSON.parseObject(source, new TypeReference<>() {});
+            return Optional.ofNullable(groupList)
+                    .map(this::extractMode)
+                    .orElse(ClusterStateManager.CLUSTER_NOT_STARTED);
+        });
+        ClusterStateManager.registerProperty(clusterModeDs.getProperty());
+    }
 
+    private int extractMode(List<ClusterGroupEntity> groupList) {
+        return ClusterStateManager.CLUSTER_CLIENT;
+    }
+
+    private Optional<ClusterClientAssignConfig> extractClientAssignment(List<ClusterGroupEntity> groupList) {
+        if (groupList.stream().anyMatch(this::machineEqual)) {
+            return Optional.empty();
+        }
+        // Build client assign config from the client set of target server group.
+        for (ClusterGroupEntity group : groupList) {
+            if (group.getClientSet().contains(getCurrentMachineId())) {
+                String ip = group.getIp();
+                Integer port = group.getPort();
+                return Optional.of(new ClusterClientAssignConfig(ip, port));
+            }
+        }
+        return Optional.empty();
+    }
+
+    private boolean machineEqual(/*@Valid*/ ClusterGroupEntity group) {
+        return getCurrentMachineId().equals(group.getMachineId());
+    }
+
+    private String getCurrentMachineId() {
+        // Note: this may not work well for container-based env.
+        return HostNameUtil.getIp() + SEPARATOR + TransportConfig.getRuntimePort();
+    }
+
+    private static final String SEPARATOR = "@";
 }
